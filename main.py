7505d9a763a6e99db4e035f1a17c57d2fc9867bc
njinjanja <milovanovic.ivana.23@sinergija.ba>
2025-07-07 18:40:12 +0200

    made print a report pretty

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from stats import count_words
 from stats import count_char_appearance
-
+from stats import sort_dict
 def get_book_text(filepath):
     with open(filepath) as f:
         return f.read()
@@ -10,8 +10,25 @@ def get_book_text(filepath):
 def main():
     book1 = get_book_text("books/frankenstein.txt")
     word_count = count_words(book1)
-    print(f"{word_count} words found in the document")
     dict_of_book=count_char_appearance(book1)
-    print(f"{dict_of_book}")
-    #print(get_book_text("books/frankenstein.txt"))
+    sorted= sort_dict(dict_of_book)
+
+
+
+    print("============ BOOKBOT ============")
+    print("Analyzing book found at books/frankenstein.txt...")
+    print("----------- Word Count ----------")
+    print(f"Found {word_count} total words")
+    print("--------- Character Count -------")
+
+    for item in sorted:
+        if item["char"].isalpha():
+            print(f'{item["char"]}: {item["num"]}')
+
+
+    print("============= END ===============")
+
+    # print(f"{word_count} words found in the document")
+    # print(f"{dict_of_book}")
+    # print(get_book_text("books/frankenstein.txt"))
 main()
--- stats.py
+++ stats.py
@@ -11,3 +11,16 @@ def count_char_appearance(booktext):
         else:
             char_dict[chara] = 1
     return char_dict
+
+def sort_on(dictionary):
+    return dictionary["num"]
+
+def sort_dict(unsorted_dict):
+    sorted_list=[]
+    for key in unsorted_dict:
+        count = unsorted_dict[key]
+        char_dict = {"char":key, "num":count}
+        sorted_list.append(char_dict)   
+    sorted_list.sort(reverse=True, key=sort_on)
+
+    return sorted_list
